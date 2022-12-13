adb3526c37169c8b990653077c755c5edac0adfb
Chakshu1760 <charliealawadhi@gmail.com>
2022-12-12 23:23:10 -0500

    added views

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const express = require('express')
 
 const app = express()
 
-app.set('view-engine','ejs')
-
 app.get('/',(req,res)=>{
     res.send("welcome to the cyclic demo app")
-})
-
-app.get('/display',(req,res)=>{
-    res.render("display.ejs",{author:'chakshu alawadhi'})
 })
 
 app.listen(3500,()=>{
