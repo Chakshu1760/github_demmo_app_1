09b1770d91f94ea136cdd905be177a20764d830b
Chakshu1760 <charliealawadhi@gmail.com>
2022-12-12 23:46:28 -0500

    added name and student number

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.port || 3500
 app.set('view-engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.send("welcome to the cyclic demo app")
+    res.send("welcome to the cyclic demo app    student name = chakshu alawadhi student number = 150056208")
 })
 
 app.get('/display',(req,res)=>{
