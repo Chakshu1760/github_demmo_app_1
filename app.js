5548c06fbc0b0a4f4dcb366cdb646b801a8cdbf8
Chakshu1760 <charliealawadhi@gmail.com>
2022-12-12 23:50:21 -0500

    csav s

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/********************************************************************************* 
+*  WEB322 – Assignment 6 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Chakshu Alawadhi Student ID: 150056208 Date: 12/12/2022 
+* 
+*  Online (Cyclic) URL: https://dull-jade-sparrow-tux.cyclic.app/display
+* 
+********************************************************************************/  
+ 
+
 const express = require('express')
 
 const app = express()
